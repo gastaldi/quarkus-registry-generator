00c4c0035a581c7326bcf2487550ec203b571b93
George Gastaldi <gegastaldi@gmail.com>
2021-08-23 19:28:05 -0300

    Support custom config

--- src/main/java/io/quarkus/registry/generator/RegistryGenerator.java
+++ src/main/java/io/quarkus/registry/generator/RegistryGenerator.java
@@ -28,7 +28,13 @@ import io.quarkus.registry.catalog.json.JsonPlatformCatalog;
 import io.quarkus.registry.catalog.json.JsonPlatformRelease;
 import io.quarkus.registry.catalog.json.JsonPlatformReleaseVersion;
 import io.quarkus.registry.catalog.json.JsonPlatformStream;
-import io.quarkus.registry.config.RegistriesConfigLocator;
+import io.quarkus.registry.config.json.JsonRegistryConfig;
+import io.quarkus.registry.config.json.JsonRegistryDescriptorConfig;
+import io.quarkus.registry.config.json.JsonRegistryMavenConfig;
+import io.quarkus.registry.config.json.JsonRegistryMavenRepoConfig;
+import io.quarkus.registry.config.json.JsonRegistryNonPlatformExtensionsConfig;
+import io.quarkus.registry.config.json.JsonRegistryPlatformsConfig;
+import io.quarkus.registry.config.json.JsonRegistryQuarkusVersionsConfig;
 import io.quarkus.registry.config.json.RegistriesConfigMapperHelper;
 import org.apache.maven.artifact.repository.metadata.Metadata;
 
@@ -53,6 +59,11 @@ public class RegistryGenerator implements Closeable {
     private static final String SHA1_EXTENSION = ".sha1";
 
     private String groupId = Constants.DEFAULT_REGISTRY_GROUP_ID;
+    private String registryId = Constants.DEFAULT_REGISTRY_ID;
+    private String registryUrl = Constants.DEFAULT_REGISTRY_MAVEN_REPO_URL;
+    private boolean supportsNonPlatforms = true;
+    private boolean quarkusVersionsExclusiveProvider;
+    private String quarkusVersionExpression;
 
     private final Date now = new Date();
 
@@ -100,6 +111,37 @@ public class RegistryGenerator implements Closeable {
     }
 
     /**
+     * Use this registry ID for all generated metadata.
+     *
+     * @param registryId the group ID to be used
+     * @return this instance, for method chaining purposes
+     */
+    public RegistryGenerator withRegistryId(String registryId) {
+        this.registryId = registryId;
+        return this;
+    }
+
+    public RegistryGenerator withRegistryUrl(String registryUrl) {
+        this.registryUrl = registryUrl;
+        return this;
+    }
+
+    public RegistryGenerator withSupportsNonPlatforms(boolean supportsNonPlatforms) {
+        this.supportsNonPlatforms = supportsNonPlatforms;
+        return this;
+    }
+
+    public RegistryGenerator withQuarkusVersionExpression(String quarkusVersionExpression) {
+        this.quarkusVersionExpression = quarkusVersionExpression;
+        return this;
+    }
+
+    public RegistryGenerator withQuarkusVersionsExclusiveProvider(boolean quarkusVersionsExclusiveProvider) {
+        this.quarkusVersionsExclusiveProvider = quarkusVersionsExclusiveProvider;
+        return this;
+    }
+
+    /**
      * Perform the generation on the given data
      *
      * @return the {@link Path} of the output directory
@@ -128,21 +170,64 @@ public class RegistryGenerator implements Closeable {
      * @throws IOException if some IO exception occurs
      */
     private void generateConfig() throws IOException {
-        var descriptorDir = createDirectories(outputDir.resolve(groupId.replace('.','/') + "/quarkus-registry-descriptor/1.0-SNAPSHOT"));
-        var contents = RegistriesConfigMapperHelper.jsonMapper().writeValueAsString(RegistriesConfigLocator.getDefaultRegistry());
+        var descriptorDir = createDirectories(
+                outputDir.resolve(groupId.replace('.', '/') + "/quarkus-registry-descriptor/1.0-SNAPSHOT"));
+        final JsonRegistryConfig qer = new JsonRegistryConfig();
+        qer.setId(registryId);
+
+        final JsonRegistryDescriptorConfig descriptor = new JsonRegistryDescriptorConfig();
+        qer.setDescriptor(descriptor);
+        descriptor.setArtifact(
+                new ArtifactCoords(groupId,
+                        Constants.DEFAULT_REGISTRY_DESCRIPTOR_ARTIFACT_ID, null,
+                        Constants.JSON, Constants.DEFAULT_REGISTRY_ARTIFACT_VERSION));
+
+        final JsonRegistryMavenConfig registryMavenConfig = new JsonRegistryMavenConfig();
+        qer.setMaven(registryMavenConfig);
+
+        final JsonRegistryPlatformsConfig platformsConfig = new JsonRegistryPlatformsConfig();
+        qer.setPlatforms(platformsConfig);
+        platformsConfig.setArtifact(new ArtifactCoords(groupId,
+                Constants.DEFAULT_REGISTRY_PLATFORMS_CATALOG_ARTIFACT_ID, null, Constants.JSON,
+                Constants.DEFAULT_REGISTRY_ARTIFACT_VERSION));
+
+        if (supportsNonPlatforms) {
+            final JsonRegistryNonPlatformExtensionsConfig nonPlatformExtensionsConfig = new JsonRegistryNonPlatformExtensionsConfig();
+            qer.setNonPlatformExtensions(nonPlatformExtensionsConfig);
+            nonPlatformExtensionsConfig.setArtifact(new ArtifactCoords(groupId,
+                    Constants.DEFAULT_REGISTRY_NON_PLATFORM_EXTENSIONS_CATALOG_ARTIFACT_ID, null, Constants.JSON,
+                    Constants.DEFAULT_REGISTRY_ARTIFACT_VERSION));
+        }
+        if (quarkusVersionExpression != null) {
+            final JsonRegistryQuarkusVersionsConfig quarkusVersionsConfig = new JsonRegistryQuarkusVersionsConfig();
+            quarkusVersionsConfig.setRecognizedVersionsExpression(quarkusVersionExpression);
+            quarkusVersionsConfig.setExclusiveProvider(quarkusVersionsExclusiveProvider);
+            qer.setQuarkusVersions(quarkusVersionsConfig);
+        }
+        final JsonRegistryMavenRepoConfig mavenRepo = new JsonRegistryMavenRepoConfig();
+        registryMavenConfig.setRepository(mavenRepo);
+        mavenRepo.setId(registryId);
+        mavenRepo.setUrl(registryUrl);
+
+        var contents = RegistriesConfigMapperHelper.jsonMapper().writeValueAsString(qer);
 
         // Generate metadata
-        Metadata metadata = generateMetadata(new ArtifactCoords(groupId, "quarkus-registry-descriptor", "1.0-SNAPSHOT"), now, Collections.emptyList());
+        Metadata metadata = generateMetadata(new ArtifactCoords(groupId, "quarkus-registry-descriptor", "1.0-SNAPSHOT"), now,
+                Collections.emptyList());
         var metadataString = MetadataGenerator.toString(metadata);
         writeString(descriptorDir.resolve("maven-metadata.xml"), metadataString);
         writeString(descriptorDir.resolve("maven-metadata.xml.sha1"), sha1(metadataString));
 
-        String timestampedJsonFile = String.format("quarkus-registry-descriptor-%s.json", metadata.getVersioning().getSnapshotVersions().get(0).getVersion());
+        String timestampedJsonFile = String.format("quarkus-registry-descriptor-%s.json",
+                metadata.getVersioning().getSnapshotVersions().get(0).getVersion());
         writeString(descriptorDir.resolve(timestampedJsonFile), contents);
         writeString(descriptorDir.resolve(timestampedJsonFile + SHA1_EXTENSION), sha1(contents));
 
-        copy(descriptorDir.resolve(timestampedJsonFile), descriptorDir.resolve("quarkus-registry-descriptor-1.0-SNAPSHOT.json"), StandardCopyOption.REPLACE_EXISTING);
-        copy(descriptorDir.resolve(timestampedJsonFile + SHA1_EXTENSION), descriptorDir.resolve("quarkus-registry-descriptor-1.0-SNAPSHOT.json" + SHA1_EXTENSION), StandardCopyOption.REPLACE_EXISTING);
+        copy(descriptorDir.resolve(timestampedJsonFile), descriptorDir.resolve("quarkus-registry-descriptor-1.0-SNAPSHOT.json"),
+                StandardCopyOption.REPLACE_EXISTING);
+        copy(descriptorDir.resolve(timestampedJsonFile + SHA1_EXTENSION),
+                descriptorDir.resolve("quarkus-registry-descriptor-1.0-SNAPSHOT.json" + SHA1_EXTENSION),
+                StandardCopyOption.REPLACE_EXISTING);
 
     }
 
@@ -157,7 +242,7 @@ public class RegistryGenerator implements Closeable {
      * @throws IOException if some IO exception occurs
      */
     private void generatePlatforms() throws IOException {
-        var descriptorDir = createDirectories(outputDir.resolve(groupId.replace('.','/') +"/quarkus-platforms/1.0-SNAPSHOT"));
+        var descriptorDir = createDirectories(outputDir.resolve(groupId.replace('.', '/') + "/quarkus-platforms/1.0-SNAPSHOT"));
 
         final JsonPlatformCatalog platformCatalog = new JsonPlatformCatalog();
 
@@ -172,7 +257,8 @@ public class RegistryGenerator implements Closeable {
 
             Map<String, JsonPlatformStream> streams = new TreeMap<>(Comparator.comparing(Version::toSortable).reversed());
             for (ExtensionCatalog catalog : catalogs) {
-                Map<String, Object> platformReleaseMetadata = (Map<String, Object>) catalog.getMetadata().get("platform-release");
+                Map<String, Object> platformReleaseMetadata = (Map<String, Object>) catalog.getMetadata()
+                        .get("platform-release");
                 String streamId = (String) platformReleaseMetadata.get("stream");
                 String version = (String) platformReleaseMetadata.get("version");
                 List<String> memberBoms = (List<String>) platformReleaseMetadata.get("members");
@@ -207,7 +293,6 @@ public class RegistryGenerator implements Closeable {
             platformCatalog.addPlatform(jsonPlatform);
         });
 
-
         // Generate maven-metadata.xml
         Set<String> quarkusVersions = platformCatalog.getPlatforms().stream()
                 .flatMap(p -> p.getStreams().stream())
@@ -215,18 +300,23 @@ public class RegistryGenerator implements Closeable {
                 .map(PlatformRelease::getQuarkusCoreVersion)
                 .collect(Collectors.toSet());
 
-        Metadata metadata = generateMetadata(new ArtifactCoords(groupId, "quarkus-platforms", "1.0-SNAPSHOT"), now, quarkusVersions);
+        Metadata metadata = generateMetadata(new ArtifactCoords(groupId, "quarkus-platforms", "1.0-SNAPSHOT"), now,
+                quarkusVersions);
         var metadataString = MetadataGenerator.toString(metadata);
         writeString(descriptorDir.resolve("maven-metadata.xml"), metadataString);
         writeString(descriptorDir.resolve("maven-metadata.xml.sha1"), sha1(metadataString));
 
-        var timestampedJsonFile = String.format("quarkus-platforms-%s.json", metadata.getVersioning().getSnapshotVersions().get(0).getVersion());
+        var timestampedJsonFile = String.format("quarkus-platforms-%s.json",
+                metadata.getVersioning().getSnapshotVersions().get(0).getVersion());
         var contents = JsonCatalogMapperHelper.mapper().writeValueAsString(platformCatalog);
         writeString(descriptorDir.resolve(timestampedJsonFile), contents);
         writeString(descriptorDir.resolve(timestampedJsonFile + SHA1_EXTENSION), sha1(contents));
 
-        copy(descriptorDir.resolve(timestampedJsonFile), descriptorDir.resolve("quarkus-platforms-1.0-SNAPSHOT.json"), StandardCopyOption.REPLACE_EXISTING);
-        copy(descriptorDir.resolve(timestampedJsonFile + SHA1_EXTENSION), descriptorDir.resolve("quarkus-platforms-1.0-SNAPSHOT.json" + SHA1_EXTENSION), StandardCopyOption.REPLACE_EXISTING);
+        copy(descriptorDir.resolve(timestampedJsonFile), descriptorDir.resolve("quarkus-platforms-1.0-SNAPSHOT.json"),
+                StandardCopyOption.REPLACE_EXISTING);
+        copy(descriptorDir.resolve(timestampedJsonFile + SHA1_EXTENSION),
+                descriptorDir.resolve("quarkus-platforms-1.0-SNAPSHOT.json" + SHA1_EXTENSION),
+                StandardCopyOption.REPLACE_EXISTING);
     }
 
     /**
@@ -242,13 +332,14 @@ public class RegistryGenerator implements Closeable {
      * @throws IOException
      */
     private void generateNonPlatformExtensions() throws IOException {
-        var descriptorDir = createDirectories(outputDir.resolve(groupId.replace('.','/') +"/quarkus-non-platform-extensions/1.0-SNAPSHOT"));
+        var descriptorDir = createDirectories(
+                outputDir.resolve(groupId.replace('.', '/') + "/quarkus-non-platform-extensions/1.0-SNAPSHOT"));
         // Generate metadata
         Metadata metadata = generateMetadata(new ArtifactCoords(groupId,
-                                                                "quarkus-non-platform-extensions",
-                                                                "1.0-SNAPSHOT"),
-                                             now,
-                                             Collections.emptyList());
+                        "quarkus-non-platform-extensions",
+                        "1.0-SNAPSHOT"),
+                now,
+                Collections.emptyList());
         String metadataString = MetadataGenerator.toString(metadata);
         writeString(descriptorDir.resolve("maven-metadata.xml"), metadataString);
         writeString(descriptorDir.resolve("maven-metadata.xml.sha1"), sha1(metadataString));
@@ -258,18 +349,23 @@ public class RegistryGenerator implements Closeable {
         for (String quarkusVersion : quarkusVersions) {
             JsonExtensionCatalog jsonExtensionCatalog = new JsonExtensionCatalog();
             jsonExtensionCatalog.setId(new ArtifactCoords(groupId,
-                                                          "quarkus-non-platform-extensions",
-                                                          quarkusVersion,
-                                                          "json",
-                                                          "1.0-SNAPSHOT").toString());
+                    "quarkus-non-platform-extensions",
+                    quarkusVersion,
+                    "json",
+                    "1.0-SNAPSHOT").toString());
             jsonExtensionCatalog.setBom(ArtifactCoords.pom("io.quarkus.platform", "quarkus-bom", quarkusVersion));
             extensionList.forEach(jsonExtensionCatalog::addExtension);
             var contents = JsonCatalogMapperHelper.mapper().writeValueAsString(jsonExtensionCatalog);
-            var timestampedJsonFile = String.format("quarkus-non-platform-extensions-%s-%s.json", metadata.getVersioning().getSnapshotVersions().get(0).getVersion(), quarkusVersion);
+            var timestampedJsonFile = String.format("quarkus-non-platform-extensions-%s-%s.json",
+                    metadata.getVersioning().getSnapshotVersions().get(0).getVersion(), quarkusVersion);
             writeString(descriptorDir.resolve(timestampedJsonFile), contents);
             writeString(descriptorDir.resolve(timestampedJsonFile + SHA1_EXTENSION), sha1(contents));
-            copy(descriptorDir.resolve(timestampedJsonFile), descriptorDir.resolve(String.format("quarkus-non-platform-extensions-1.0-SNAPSHOT-%s.json", quarkusVersion)), StandardCopyOption.REPLACE_EXISTING);
-            copy(descriptorDir.resolve(timestampedJsonFile + SHA1_EXTENSION), descriptorDir.resolve(String.format("quarkus-non-platform-extensions-1.0-SNAPSHOT-%s.json" + SHA1_EXTENSION, quarkusVersion)), StandardCopyOption.REPLACE_EXISTING);
+            copy(descriptorDir.resolve(timestampedJsonFile), descriptorDir.resolve(
+                            String.format("quarkus-non-platform-extensions-1.0-SNAPSHOT-%s.json", quarkusVersion)),
+                    StandardCopyOption.REPLACE_EXISTING);
+            copy(descriptorDir.resolve(timestampedJsonFile + SHA1_EXTENSION), descriptorDir.resolve(
+                            String.format("quarkus-non-platform-extensions-1.0-SNAPSHOT-%s.json" + SHA1_EXTENSION, quarkusVersion)),
+                    StandardCopyOption.REPLACE_EXISTING);
 
         }
     }
--- src/test/java/io/quarkus/registry/generator/RegistryGeneratorTest.java
+++ src/test/java/io/quarkus/registry/generator/RegistryGeneratorTest.java
@@ -10,8 +10,11 @@ import io.quarkus.registry.catalog.PlatformRelease;
 import io.quarkus.registry.catalog.PlatformStream;
 import io.quarkus.registry.catalog.json.JsonCatalogMapperHelper;
 import io.quarkus.registry.catalog.json.JsonPlatformCatalog;
-import io.quarkus.registry.catalog.json.JsonPlatformRelease;
 import io.quarkus.registry.catalog.json.JsonPlatformReleaseVersion;
+import io.quarkus.registry.config.RegistriesConfig;
+import io.quarkus.registry.config.RegistryConfig;
+import io.quarkus.registry.config.json.JsonRegistryConfig;
+import io.quarkus.registry.config.json.RegistriesConfigMapperHelper;
 import org.apache.maven.artifact.repository.metadata.io.xpp3.MetadataXpp3Reader;
 import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
 import org.junit.jupiter.api.Test;
@@ -24,9 +27,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 class RegistryGeneratorTest {
 
     @Test
-    void should_use_custom_group_id(@TempDir Path tempDir) throws Exception {
+    void should_use_custom_settings(@TempDir Path tempDir) throws Exception {
         Path path = new RegistryGenerator(tempDir)
                 .withGroupId("lorem.ipsum.dolor")
+                .withRegistryId("foo.bar")
+                .withRegistryUrl("https://bar.foo.com")
+                .withQuarkusVersionExpression("1.0.0")
+                .withQuarkusVersionsExclusiveProvider(true)
+                .withSupportsNonPlatforms(false)
                 .generate();
         Path registryDescriptorRoot = path.resolve("lorem/ipsum/dolor/quarkus-registry-descriptor/1.0-SNAPSHOT/");
         assertThat(registryDescriptorRoot).exists();
@@ -38,6 +46,16 @@ class RegistryGeneratorTest {
         assertThat(registryDescriptorRoot.resolve(String.format("quarkus-registry-descriptor-%s.json.sha1", version))).exists();
         assertThat(registryDescriptorRoot.resolve("quarkus-registry-descriptor-1.0-SNAPSHOT.json")).exists();
         assertThat(registryDescriptorRoot.resolve("quarkus-registry-descriptor-1.0-SNAPSHOT.json.sha1")).exists();
+
+        RegistryConfig registryConfig = RegistriesConfigMapperHelper.jsonMapper()
+                .readValue(registryDescriptorRoot.resolve("quarkus-registry-descriptor-1.0-SNAPSHOT.json").toFile(),
+                        JsonRegistryConfig.class);
+        // id is marked with @JsonIgnore
+        //assertThat(registryConfig.getId()).isEqualTo("foo.bar");
+        assertThat(registryConfig.getNonPlatformExtensions()).isNull();
+        assertThat(registryConfig.getQuarkusVersions().getRecognizedVersionsExpression()).isEqualTo("1.0.0");
+        assertThat(registryConfig.getQuarkusVersions().isExclusiveProvider()).isTrue();
+        assertThat(registryConfig.getMaven().getRepository().getUrl()).isEqualTo("https://bar.foo.com");
     }
 
     @Test
@@ -59,10 +77,10 @@ class RegistryGeneratorTest {
     @Test
     void should_generate_platform_descriptor_and_maven_metadata(@TempDir Path tempDir) throws Exception {
         ExtensionCatalog extensionCatalog = extractExtensionCatalog(MetadataExtractor.MAVEN_CENTRAL,
-                                                                    "io.quarkus.platform",
-                                                                    "quarkus-bom-quarkus-platform-descriptor",
-                                                                    "2.0.3.Final",
-                                                                    "2.0.3.Final");
+                "io.quarkus.platform",
+                "quarkus-bom-quarkus-platform-descriptor",
+                "2.0.3.Final",
+                "2.0.3.Final");
         Path path = new RegistryGenerator(tempDir)
                 .add(extensionCatalog)
                 .generate();
@@ -79,11 +97,12 @@ class RegistryGeneratorTest {
     @Test
     void should_generate_non_platform_descriptor_and_maven_metadata(@TempDir Path tempDir) throws Exception {
         ExtensionCatalog extensionCatalog = extractExtensionCatalog(MetadataExtractor.MAVEN_CENTRAL,
-                                                                    "io.quarkus.platform",
-                                                                    "quarkus-bom-quarkus-platform-descriptor",
-                                                                    "2.0.3.Final",
-                                                                    "2.0.3.Final");
-        Extension extension = extractExtension(MetadataExtractor.MAVEN_CENTRAL, "io.quarkiverse.prettytime", "quarkus-prettytime", "0.1.1");
+                "io.quarkus.platform",
+                "quarkus-bom-quarkus-platform-descriptor",
+                "2.0.3.Final",
+                "2.0.3.Final");
+        Extension extension = extractExtension(MetadataExtractor.MAVEN_CENTRAL, "io.quarkiverse.prettytime",
+                "quarkus-prettytime", "0.1.1");
         Path path = new RegistryGenerator(tempDir)
                 .add(extensionCatalog)
                 .add(extension)
@@ -95,39 +114,44 @@ class RegistryGeneratorTest {
         assertThat(nonPlatformExtensionsRoot.resolve("maven-metadata.xml.sha1")).exists();
 
         String version = getMetadataVersion(nonPlatformExtensionsRoot.resolve("maven-metadata.xml"));
-        assertThat(nonPlatformExtensionsRoot.resolve(String.format("quarkus-non-platform-extensions-%s-2.0.3.Final.json", version))).exists();
-        assertThat(nonPlatformExtensionsRoot.resolve(String.format("quarkus-non-platform-extensions-%s-2.0.3.Final.json.sha1", version))).exists();
+        assertThat(nonPlatformExtensionsRoot.resolve(
+                String.format("quarkus-non-platform-extensions-%s-2.0.3.Final.json", version))).exists();
+        assertThat(nonPlatformExtensionsRoot.resolve(
+                String.format("quarkus-non-platform-extensions-%s-2.0.3.Final.json.sha1", version))).exists();
         assertThat(nonPlatformExtensionsRoot.resolve("quarkus-non-platform-extensions-1.0-SNAPSHOT-2.0.3.Final.json")).exists();
-        assertThat(nonPlatformExtensionsRoot.resolve("quarkus-non-platform-extensions-1.0-SNAPSHOT-2.0.3.Final.json.sha1")).exists();
+        assertThat(nonPlatformExtensionsRoot.resolve(
+                "quarkus-non-platform-extensions-1.0-SNAPSHOT-2.0.3.Final.json.sha1")).exists();
     }
 
     @Test
     void should_order_platform_streams_and_releases(@TempDir Path tempDir) throws Exception {
         Path path = new RegistryGenerator(tempDir)
                 .add(extractExtensionCatalog(MetadataExtractor.MAVEN_CENTRAL,
-                                             "io.quarkus.platform",
-                                             "quarkus-bom-quarkus-platform-descriptor",
-                                             "2.0.2.Final",
-                                             "2.0.2.Final"))
-                .add(extractExtension(MetadataExtractor.MAVEN_CENTRAL, "io.quarkiverse.prettytime", "quarkus-prettytime", "0.1.0"))
+                        "io.quarkus.platform",
+                        "quarkus-bom-quarkus-platform-descriptor",
+                        "2.0.2.Final",
+                        "2.0.2.Final"))
+                .add(extractExtension(MetadataExtractor.MAVEN_CENTRAL, "io.quarkiverse.prettytime", "quarkus-prettytime",
+                        "0.1.0"))
                 .add(extractExtensionCatalog(MetadataExtractor.MAVEN_CENTRAL,
-                                             "io.quarkus.platform",
-                                             "quarkus-bom-quarkus-platform-descriptor",
-                                             "2.0.3.Final",
-                                             "2.0.3.Final"))
+                        "io.quarkus.platform",
+                        "quarkus-bom-quarkus-platform-descriptor",
+                        "2.0.3.Final",
+                        "2.0.3.Final"))
                 .add(extractExtensionCatalog(MetadataExtractor.MAVEN_CENTRAL,
-                                             "io.quarkus.platform",
-                                             "quarkus-bom-quarkus-platform-descriptor",
-                                             "2.1.0.CR1",
-                                             "2.1.0.CR1"))
+                        "io.quarkus.platform",
+                        "quarkus-bom-quarkus-platform-descriptor",
+                        "2.1.0.CR1",
+                        "2.1.0.CR1"))
                 .add(extractExtensionCatalog(MetadataExtractor.MAVEN_CENTRAL,
-                                             "io.quarkus.platform",
-                                             "quarkus-bom-quarkus-platform-descriptor",
-                                             "2.1.1.Final",
-                                             "2.1.1.Final"))
+                        "io.quarkus.platform",
+                        "quarkus-bom-quarkus-platform-descriptor",
+                        "2.1.1.Final",
+                        "2.1.1.Final"))
                 .generate();
         Path platformDescriptorRoot = path.resolve("io/quarkus/registry/quarkus-platforms/1.0-SNAPSHOT");
-        JsonPlatformCatalog platformCatalog = JsonCatalogMapperHelper.deserialize(platformDescriptorRoot.resolve("quarkus-platforms-1.0-SNAPSHOT.json"), JsonPlatformCatalog.class);
+        JsonPlatformCatalog platformCatalog = JsonCatalogMapperHelper.deserialize(
+                platformDescriptorRoot.resolve("quarkus-platforms-1.0-SNAPSHOT.json"), JsonPlatformCatalog.class);
         assertThat(platformCatalog.getPlatforms()).hasSize(1);
         assertThat(platformCatalog.getRecommendedPlatform().getStreams())
                 .extracting(PlatformStream::getId)
@@ -138,9 +162,9 @@ class RegistryGeneratorTest {
                 .extracting(PlatformRelease::getVersion).isEqualTo(JsonPlatformReleaseVersion.fromString("2.0.3.Final"));
     }
 
-
     private String getMetadataVersion(Path metadataPath) throws IOException, XmlPullParserException {
-        return new MetadataXpp3Reader().read(Files.newBufferedReader(metadataPath)).getVersioning().getSnapshotVersions().get(0).getVersion();
+        return new MetadataXpp3Reader().read(Files.newBufferedReader(metadataPath)).getVersioning().getSnapshotVersions().get(0)
+                .getVersion();
     }
 
 }
